ef9f9b03c494444e4184e83babac013bcae68af8
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-16 22:21:16 -0400

    style: Remove extra whitespace from script output

--- logparser-check.ps1
+++ logparser-check.ps1
@@ -1,13 +1,13 @@
 # =======================
 Write-Host " "
 Write-Host " "
-Write-Host "Pre-Requisite Check for the Logparser.exe on local path" -NoNewline
+Write-Host "Checking for the Logparser.exe on local path..." -NoNewline
 
 Start-Sleep -Seconds 2
 If(Test-Path -Path Logparser.exe){
-Write-Host "                 [   OK   ]" -ForegroundColor Green
+Write-Host "[   OK   ]" -ForegroundColor Green
 } Else {
-Write-Host "                 [ Failed ]" -ForegroundColor Red
+Write-Host "[ Failed ]" -ForegroundColor Red
 Write-Host " "
 Write-Host " "
 Write-Warning "Either Logparser is not installed or you are running this script on a different folder where Logparser.exe file is unavailable."
