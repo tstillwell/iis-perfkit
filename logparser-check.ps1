353dbbcac5cf03813148eda747bb94214816704b
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-16 23:01:06 -0400

    style: Remove LogParser download link message

--- logparser-check.ps1
+++ logparser-check.ps1
@@ -12,8 +12,5 @@ Write-Host " "
 Write-Host " "
 Write-Warning "Either Logparser is not installed or you are running this script on a different folder where Logparser.exe file is unavailable."
 Write-Host " "
-Write-Host "To download logparser follow this link : " -NoNewline -BackgroundColor Yellow -ForegroundColor Black
-Write-Host "http://www.microsoft.com/en-in/download/details.aspx?id=24659" -ForegroundColor Blue -BackgroundColor Yellow
-Write-Host " "
 exit
 }
