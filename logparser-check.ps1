1fe16dde7d3a54f5ce5e285c70b2874c5c6bacd6
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-17 23:04:42 -0400

    style: Add more whitespace to lp check for readability

--- logparser-check.ps1
+++ logparser-check.ps1
@@ -7,14 +7,14 @@ Write-Host " "
 Write-Host "Checking for the Logparser.exe on local path..." -NoNewline
 
 Start-Sleep -Seconds 2
-If(Test-Path -Path Logparser.exe){
-Write-Host "[   OK   ]" -ForegroundColor Green
+If (Test-Path -Path Logparser.exe){
+  Write-Host "[   OK   ]" -ForegroundColor Green
 } 
 Else {
-Write-Host "[ Failed ]" -ForegroundColor Red
-Write-Host " "
-Write-Host " "
-Write-Warning "Either Logparser is not installed or you are running this script on a different folder where Logparser.exe file is unavailable."
-Write-Host " "
-exit
+  Write-Host "[ Failed ]" -ForegroundColor Red
+  Write-Host " "
+  Write-Host " "
+  Write-Warning "Either Logparser is not installed or you are running this script on a different folder where Logparser.exe file is unavailable."
+  Write-Host " "
+  exit
 }
