33f566c2b4ab3aa5336a7aedfda47e9695cf4216
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-18 23:06:55 -0400

    feat: Add parser scripts directory

---
+++ parser-scripts/hitsbyip.sql
@@ -0,0 +1,12 @@
+-- Number of Hits per Client IP, including a Reverse DNS lookup (SLOW)
+
+ 	SELECT c-ip As Machine, 
+    REVERSEDNS(c-ip) As Name, 
+    COUNT(*) As Hits 
+ FROM c:\inetpub\logs\LogFiles\W3SVC1\* 
+ GROUP BY Machine ORDER BY Hits DESC
+ 
+-- Machine 	    Name 	       Hits
+-- ::1 	        CARLOSAGDEV 	57
+-- 127.0.0.1 	MACHINE1 	    28
+-- 27.X.X.X 	MACHINE2 	     1
