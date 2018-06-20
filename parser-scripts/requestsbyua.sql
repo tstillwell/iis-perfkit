0d8005f9e6227fe608977620d44d707a385fcb51
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-19 21:10:43 -0400

    feat: Add requests by user-agent sql script

---
+++ parser-scripts/requestsbyua.sql
@@ -0,0 +1,12 @@
+--List all the requests by user agent
+SELECT 
+    cs(User-Agent) As UserAgent, 
+    COUNT(*) as Hits 
+FROM c:\inetpub\logs\LogFiles\W3SVC1\* 
+GROUP BY UserAgent 
+ORDER BY Hits DESC 	
+--UserAgent 	Hits
+--iisbot/1.0+(+http://www.iis.net/iisbot.html) 	104
+--Mozilla/4.0+(compatible;+MSIE+8.0;… 	77
+--Microsoft-WebDAV-MiniRedir/6.1.7600 	23
+--DavClnt 	1
