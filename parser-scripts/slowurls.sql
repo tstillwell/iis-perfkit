1ae87b84ccd4986a5bfd78c693326545fa1e0da5
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-20 21:54:27 -0400

    style: Make spacing consistent in slowurls.sql

--- parser-scripts/slowurls.sql
+++ parser-scripts/slowurls.sql
@@ -1,4 +1,4 @@
---Find the Slowest 25 URLs (in average) in the site
+--  Find the Slowest 25 URLs (in average) in the site
 SELECT TOP 25 
     cs-uri-stem as URL, 
     MAX(time-taken) As Max, 
@@ -7,9 +7,9 @@ SELECT TOP 25
 FROM c:\inetpub\logs\LogFiles\W3SVC1\* 
 GROUP BY URL 
 ORDER By Average DESC 	
--- URL 	                   Max 	   Min 	 Average
--- /Test/Default.aspx 	    23215  23215   23215
--- /WebSite/Default.aspx 	5757     752 	4178
--- /Remote2008.jpg 	        3510 	3510 	3510
--- /wordpress/ 	            6541 	   2    3271
--- /RemoteVista.jpg 	    3314 	   2    1658
+--  URL 	                   Max 	   Min 	 Average
+--  /Test/Default.aspx 	     23215   23215     23215
+--  /WebSite/Default.aspx 	  5757     752 	    4178
+--  /Remote2008.jpg 	      3510 	  3510 	    3510
+--  /wordpress/ 	          6541 	     2      3271
+--  /RemoteVista.jpg 	      3314 	     2      1658
