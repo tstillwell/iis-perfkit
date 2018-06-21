bf848c46f1a0295e46a6e13f671ef29d00dbe34b
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-20 21:57:00 -0400

    style: Make spacing consistent in hitspermethod.sql

--- parser-scripts/hitspermethod.sql
+++ parser-scripts/hitspermethod.sql
@@ -1,12 +1,12 @@
---Number of hits per Method (GET, POST, etc) 	
+--  Number of hits per Method (GET, POST, etc) 	
 SELECT 
     cs-method As Method, 
     COUNT(*) As Hits 
 FROM c:\inetpub\logs\LogFiles\W3SVC1\* 
 GROUP BY Method 	
---Method 	Hits
---GET 	  133566
---POST 	   10901
---HEAD 	     568
---OPTIONS 	  11
---PROPFIND 	  18
+--  Method 	  Hits
+--  GET 	  133566
+--  POST 	   10901
+--  HEAD 	     568
+--  OPTIONS 	  11
+--  PROPFIND 	  18
