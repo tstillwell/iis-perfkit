7888f6838d72c86eed89fcea67ad6dfb12f8650b
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-20 21:35:27 -0400

    style: Make spacing consistent in topfiletypes.sql

--- parser-scripts/topfiletypes.sql
+++ parser-scripts/topfiletypes.sql
@@ -5,9 +5,9 @@ SELECT TOP 25
 FROM c:\inetpub\logs\LogFiles\W3SVC1\* 
 GROUP BY Extension 
 ORDER BY Hits DESC 	
---Extension 	Hits
---gif 	    52127
---bmp 	    20377
---axd 	    10321
---txt 	    460
---htm 	    362
+--  Extension 	Hits
+--  gif 	    52127
+--  bmp 	    20377
+--  axd 	    10321
+--  txt 	    460
+--  htm 	    362
