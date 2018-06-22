e7d2b684a3dcc141fed8016cdbcd3fd3453d46af
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-21 23:21:41 -0400

    style: Replace tabs with spaces in requestsbyua.sql

--- parser-scripts/requestsbyua.sql
+++ parser-scripts/requestsbyua.sql
@@ -5,8 +5,8 @@ SELECT
 FROM c:\inetpub\logs\LogFiles\W3SVC1\* 
 GROUP BY UserAgent 
 ORDER BY Hits DESC 	
---  UserAgent 	Hits
---  iisbot/1.0+(+http://www.iis.net/iisbot.html) 	104
---  Mozilla/4.0+(compatible;+MSIE+8.0;… 	77
---  Microsoft-WebDAV-MiniRedir/6.1.7600 	23
---  DavClnt 	1
+--  UserAgent   Hits
+--  iisbot/1.0+(+http://www.iis.net/iisbot.html)    104
+--  Mozilla/4.0+(compatible;+MSIE+8.0;      77
+--  Microsoft-WebDAV-MiniRedir/6.1.7600     23
+--  DavClnt     1
