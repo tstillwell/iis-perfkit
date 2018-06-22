83f2c73a8d67e2309b596769863121406542860e
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-21 23:23:29 -0400

    style: Replace tabs with spaces in slowurls.sql

--- parser-scripts/slowurls.sql
+++ parser-scripts/slowurls.sql
@@ -7,9 +7,9 @@ SELECT TOP 25
 FROM c:\inetpub\logs\LogFiles\W3SVC1\* 
 GROUP BY URL 
 ORDER By Average DESC 	
---  URL 	                   Max 	   Min 	 Average
---  /Test/Default.aspx 	     23215   23215     23215
---  /WebSite/Default.aspx 	  5757     752 	    4178
---  /Remote2008.jpg 	      3510 	  3510 	    3510
---  /wordpress/ 	          6541 	     2      3271
---  /RemoteVista.jpg 	      3314 	     2      1658
+--  URL                        Max     Min   Average
+--  /Test/Default.aspx       23215   23215     23215
+--  /WebSite/Default.aspx     5757     752      4178
+--  /Remote2008.jpg           3510    3510      3510
+--  /wordpress/               6541       2      3271
+--  /RemoteVista.jpg          3314       2      1658
