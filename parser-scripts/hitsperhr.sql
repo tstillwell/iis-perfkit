eec5f8909fca62a06159f80e6c331a33680cb791
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-22 23:50:42 -0400

    style: Replace tabs with spaces in hitsperhr.sql

--- parser-scripts/hitsperhr.sql
+++ parser-scripts/hitsperhr.sql
@@ -1,11 +1,11 @@
---  Number of hits per hour for the month of March 	
+--  Number of hits per hour for the month of March
 SELECT 
     QUANTIZE(TO_LOCALTIME(TO_TIMESTAMP(date, time)), 3600) AS Hour, 
     COUNT(*) AS Hits 
 FROM c:\inetpub\logs\LogFiles\W3SVC1\* 
 WHERE date>'2010-03-01' and date<'2010-04-01' 
-Group By Hour 	
---  Date         Hour 	  	 Hits
---  3/3/2010 	 10:00:00 	 33
---  3/3/2010 	 11:00:00 	 5
---  3/3/2010 	 12:00:00 	 3
+Group By Hour 
+--  Date         Hour        Hits
+--  3/3/2010     10:00:00    33
+--  3/3/2010     11:00:00    5
+--  3/3/2010     12:00:00    3
