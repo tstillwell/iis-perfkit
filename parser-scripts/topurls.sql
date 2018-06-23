62e7c93e1555aab2f9656964013c566bb7de843d
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-22 23:19:30 -0400

    style: Replace tabs with spaces in topurls.sql

--- parser-scripts/topurls.sql
+++ parser-scripts/topurls.sql
@@ -1,13 +1,13 @@
---  Top 25 URLs 	
+--  Top 25 URLs
 SELECT TOP 25 
     cs-uri-stem as Url, 
     COUNT(*) As Hits 
 FROM c:\inetpub\logs\LogFiles\W3SVC1\* 
 GROUP BY cs-uri-stem 
 ORDER By Hits DESC 	
---  Url 	  Hits
---  /WebResource.axd 	10318
---  /favicon.ico 	8523
---  /Tools/CodeTranslator/Translate.ashx 	6519
---  /App_Themes/Silver/carlosag.css 	5898
---  /images/arrow.gif 	5720
+--  Url       Hits
+--  /WebResource.axd     10318
+--  /favicon.ico     8523
+--  /Tools/CodeTranslator/Translate.ashx     6519
+--  /App_Themes/Silver/carlosag.css     5898
+--  /images/arrow.gif     5720
