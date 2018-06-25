17641964af8666990e23380e4261e3af67298af2
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-24 22:24:20 -0400

    feat: Run scripts synchronously to avoid output mixing

--- perfkit.ps1
+++ perfkit.ps1
@@ -1,5 +1,5 @@
 GET-ChildItem parser-scripts |
   ForEach-Object { # run logparser with each script
     $cmdargs = "-i IISW3C file:parser-scripts/$_"
-    Start-Process -FilePath ".\Logparser.exe " -ArgumentList "$cmdargs" -NoNewWindow
+    Start-Process -FilePath ".\Logparser.exe " -ArgumentList "$cmdargs" -NoNewWindow -Wait
   }
