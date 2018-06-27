a687f80ceacbf3b73054d5e0a3401f697a2b584f
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-26 22:32:04 -0400

    feat: Add log_path var as arg to each sql script

--- perfkit.ps1
+++ perfkit.ps1
@@ -1,5 +1,7 @@
 GET-ChildItem parser-scripts |
   ForEach-Object { # run logparser with each script
-    $cmdargs = "-i IISW3C file:parser-scripts/$_"
+    $log_path =   "c:\inetpub\logs\LogFiles\W3SVC1\*"
+    $cmdargs = "-i IISW3C file:parser-scripts/$_?log_path=$log_path"
     Start-Process -FilePath ".\Logparser.exe " -ArgumentList "$cmdargs" -NoNewWindow -Wait
   }
+  
