9c092fce00c7b77ade1a4005d732d42b8229d271
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-28 22:01:56 -0400

    refactor: Add $script variable for readability

--- perfkit.ps1
+++ perfkit.ps1
@@ -1,7 +1,8 @@
 GET-ChildItem parser-scripts |
   ForEach-Object { # run logparser with each script
     $log_path =   "c:\inetpub\logs\LogFiles\W3SVC1\*"
-    $cmdargs = "-i IISW3C file:parser-scripts/$_?log_path=$log_path"
+    $script = "$_" # filename of the current script
+    $cmdargs = "-i IISW3C file:parser-scripts/$script?log_path=$log_path"
     Start-Process -FilePath ".\Logparser.exe " -ArgumentList "$cmdargs" -NoNewWindow -Wait
   }
   
