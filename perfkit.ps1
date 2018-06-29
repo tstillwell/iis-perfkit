a49b2442448938a0381d12341f0260f3dcc46797
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-28 22:04:30 -0400

    docs: Add line comment for $cmdargs variable

--- perfkit.ps1
+++ perfkit.ps1
@@ -2,6 +2,7 @@ GET-ChildItem parser-scripts |
   ForEach-Object { # run logparser with each script
     $log_path =   "c:\inetpub\logs\LogFiles\W3SVC1\*"
     $script = "$_" # filename of the current script
+    # logparser CLI arguments - see logparser help for more info
     $cmdargs = "-i IISW3C file:parser-scripts/$script?log_path=$log_path"
     Start-Process -FilePath ".\Logparser.exe " -ArgumentList "$cmdargs" -NoNewWindow -Wait
   }
