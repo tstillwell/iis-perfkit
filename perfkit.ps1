af77afe9ef13976888d16a4b09b91b5514a1c4c2
tstillwell2k17 <tristanstillwell@gmail.com>
2018-06-30 21:21:23 -0400

    style: Remove extra whitespace from log_path line

--- perfkit.ps1
+++ perfkit.ps1
@@ -1,7 +1,7 @@
 GET-ChildItem -Path parser-scripts\*.sql |
   ForEach-Object { # run logparser with each script
     # location of logfiles to query
-    $log_path =   "c:\inetpub\logs\LogFiles\W3SVC1\*"
+    $log_path = "c:\inetpub\logs\LogFiles\W3SVC1\*"
     # filename of the current script
     $script = "$_"
     # logparser CLI arguments - see logparser help for more info
