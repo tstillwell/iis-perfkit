44a7a12dc94eaf8de632202d0eb1cdd89c9ce387
tstillwell2k17 <tristanstillwell@gmail.com>
2018-07-03 23:43:40 -0400

    docs: Add attribution link to hitsbyip.sql

--- parser-scripts/hitsbyip.sql
+++ parser-scripts/hitsbyip.sql
@@ -1,4 +1,5 @@
 -- Number of Hits per Client IP, including a Reverse DNS lookup (SLOW)
+-- https://blogs.msdn.microsoft.com/carlosag/2010/03/25/analyze-your-iis-log-files-favorite-log-parser-queries/
 SELECT c-ip As Machine, 
     REVERSEDNS(c-ip) As Name, 
     COUNT(*) As Hits 
