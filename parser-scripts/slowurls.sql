0c2b646837592ff7aac4ae9ab7d6eea8c17e92fb
tstillwell2k17 <tristanstillwell@gmail.com>
2018-07-03 23:45:51 -0400

    docs: Add attribution link to slowurls.sql

--- parser-scripts/slowurls.sql
+++ parser-scripts/slowurls.sql
@@ -1,4 +1,5 @@
 --  Find the Slowest 25 URLs (in average) in the site
+-- https://blogs.msdn.microsoft.com/carlosag/2010/03/25/analyze-your-iis-log-files-favorite-log-parser-queries/
 SELECT TOP 25 
     cs-uri-stem as URL, 
     MAX(time-taken) As Max, 
