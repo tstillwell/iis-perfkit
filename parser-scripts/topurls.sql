9bd6f3610d212ee1b8a3a675cd4f6ac236a693d3
tstillwell2k17 <tristanstillwell@gmail.com>
2018-07-06 22:24:45 -0400

    docs: Add attribuition link to topfiles.sql

--- parser-scripts/topurls.sql
+++ parser-scripts/topurls.sql
@@ -1,4 +1,5 @@
 --  Top 25 URLs
+-- https://blogs.msdn.microsoft.com/carlosag/2010/03/25/analyze-your-iis-log-files-favorite-log-parser-queries/
 SELECT TOP 25 
     cs-uri-stem as Url, 
     COUNT(*) As Hits 
