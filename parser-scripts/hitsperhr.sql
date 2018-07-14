6a1c54f558db62637c07e6a60fa1b52c87555cf8
tstillwell2k17 <tristanstillwell@gmail.com>
2018-07-13 22:05:17 -0400

    style: Trim excess whitespace from query in hitsperhr.sql

--- parser-scripts/hitsperhr.sql
+++ parser-scripts/hitsperhr.sql
@@ -1,11 +1,11 @@
 -- Number of hits per hour for the month of March
 -- https://blogs.msdn.microsoft.com/carlosag/
-SELECT 
-    QUANTIZE(TO_LOCALTIME(TO_TIMESTAMP(date, time)), 3600) AS Hour, 
-    COUNT(*) AS Hits 
+SELECT
+    QUANTIZE(TO_LOCALTIME(TO_TIMESTAMP(date, time)), 3600) AS Hour,
+    COUNT(*) AS Hits
 FROM %log_path%
-WHERE date > '2010-03-01' AND date < '2010-04-01' 
-GROUP BY Hour 
+WHERE date > '2010-03-01' AND date < '2010-04-01'
+GROUP BY Hour
 --  Date         Hour        Hits
 --  3/3/2010     10:00:00    33
 --  3/3/2010     11:00:00    5
