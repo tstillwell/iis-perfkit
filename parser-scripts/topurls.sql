30d303256dde729ffb5c4e8c5dceb69455f3a3f2
tstillwell2k17 <tristanstillwell@gmail.com>
2018-07-13 22:00:23 -0400

    style: Trim excess whitespace from query in topurls.sql

--- parser-scripts/topurls.sql
+++ parser-scripts/topurls.sql
@@ -1,11 +1,11 @@
 -- Top URLs by hit count
 -- https://blogs.msdn.microsoft.com/carlosag/
 SELECT
-    cs-uri-stem AS Url, 
-    COUNT(*) AS Hits 
+    cs-uri-stem AS Url,
+    COUNT(*) AS Hits
 FROM %log_path%
-GROUP BY cs-uri-stem 
-ORDER BY Hits DESC 
+GROUP BY cs-uri-stem
+ORDER BY Hits DESC
 --  Url       Hits
 --  /WebResource.axd     10318
 --  /favicon.ico     8523
