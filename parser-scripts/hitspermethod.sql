f0a4937a8bec6544756a5fd23a77097dd89a2a52
tstillwell2k17 <tristanstillwell@gmail.com>
2018-07-13 22:04:19 -0400

    style: Trim excess whitespace from query in hitspermethod.sql

--- parser-scripts/hitspermethod.sql
+++ parser-scripts/hitspermethod.sql
@@ -1,10 +1,10 @@
 -- Number of hits per Method (GET, POST, etc)
 -- https://blogs.msdn.microsoft.com/carlosag/
-SELECT 
-    cs-method AS Method, 
-    COUNT(*) AS Hits 
+SELECT
+    cs-method AS Method,
+    COUNT(*) AS Hits
 FROM %log_path%
-GROUP BY Method 
+GROUP BY Method
 --  Method    Hits
 --  GET       133566
 --  POST       10901
