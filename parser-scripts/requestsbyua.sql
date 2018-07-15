947afc84844f62a9c457e371cc638ca9b34d418e
tstillwell2k17 <tristanstillwell@gmail.com>
2018-07-14 21:41:01 -0400

    style: Trim excess whitespace from query in requestsbyua.sql

--- parser-scripts/requestsbyua.sql
+++ parser-scripts/requestsbyua.sql
@@ -1,11 +1,11 @@
 -- List all the requests by user agent
 -- https://blogs.msdn.microsoft.com/carlosag/
-SELECT 
-    cs(User-Agent) AS UserAgent, 
-    COUNT(*) AS Hits 
-FROM %log_path% 
-GROUP BY UserAgent 
-ORDER BY Hits DESC 
+SELECT
+    cs(User-Agent) AS UserAgent,
+    COUNT(*) AS Hits
+FROM %log_path%
+GROUP BY UserAgent
+ORDER BY Hits DESC
 --  UserAgent   Hits
 --  iisbot/1.0+(+http://www.iis.net/iisbot.html)    104
 --  Mozilla/4.0+(compatible;+MSIE+8.0;      77
