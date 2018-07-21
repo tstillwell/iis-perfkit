af0ad335961857cb133c2ed242c5be85c9dd66c9
tstillwell2k17 <tristanstillwell@gmail.com>
2018-07-20 23:02:16 -0400

    docs: Rephrase comment on Get-ChildItem line

--- perfkit.ps1
+++ perfkit.ps1
@@ -3,10 +3,10 @@
 # https://github.com/tstillwell/iis-perfkit
 # usage: > perfkit.ps1
 # See README for more info
-GET-ChildItem -Path parser-scripts\*.sql | # all sql files in parser-scripts
+GET-ChildItem -Path parser-scripts\*.sql | # all sql scripts in parser-scripts
   ForEach-Object {
     $log_path = "c:\inetpub\logs\LogFiles\W3SVC1\*"
-    $script = "$_"  # filename of the current script
+    $script = "$_" # filename of the current script
     # logparser CLI arguments - see logparser help for more info
     $cmdargs = "-i IISW3C file:$($script)?log_path=$log_path"
     # run each logparser command
